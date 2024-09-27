029b89776df01a807117a1d197b30f647f8bbae2
Diego Szychowski <diegos@exo.com.ar>
2024-09-27 13:24:52 -0300

    chore: add git-chglog tool

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -7,16 +7,22 @@ GO     ?= $(shell which go)
 
 # Below generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
-# For example for svu variable:
+# For example for git-chglog variable:
 #
 # In your main Makefile (for non array binaries):
 #
 #include .bingo/Variables.mk # Assuming -dir was set to .bingo .
 #
-#command: $(SVU)
-#	@echo "Running svu"
-#	@$(SVU) <flags/args..>
+#command: $(GIT_CHGLOG)
+#	@echo "Running git-chglog"
+#	@$(GIT_CHGLOG) <flags/args..>
 #
+GIT_CHGLOG := $(GOBIN)/git-chglog-v0.15.4
+$(GIT_CHGLOG): $(BINGO_DIR)/git-chglog.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/git-chglog-v0.15.4"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=git-chglog.mod -o=$(GOBIN)/git-chglog-v0.15.4 "github.com/git-chglog/git-chglog/cmd/git-chglog"
+
 SVU := $(GOBIN)/svu-v1.12.0
 $(SVU): $(BINGO_DIR)/svu.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
